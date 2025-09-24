9a7fe1f8720bbc0b9738c297709cd229e30100b3
HuynhTrungKien <hkien666@gmail.com>
2025-09-24 18:57:46 +0700

    Update BCD.v

--- sources_1/new/BCD.v
+++ sources_1/new/BCD.v
@@ -27,8 +27,8 @@ module BCD(
     input [3:0] tens,
     input [3:0] hundreds,
     input [3:0] thousands,
-    output reg [6:0] seg,       // segment pattern 0-9
-    output reg [3:0] digit      // digit select signals
+    output reg [6:0] seg,     
+    output reg [3:0] digit     
     );
     
 // Cathode chung: 1 = sáng, 0 = tắt
@@ -49,36 +49,36 @@ parameter D_HEX = 7'b101_1110;  // d
 parameter E_HEX = 7'b111_1001;  // E
 parameter F_HEX = 7'b111_0001;  // F
     
-    // To select each digit in turn
-    reg [1:0] digit_select;     // 2 bit counter for selecting each of 4 digits
-    reg [16:0] digit_timer;     // counter for digit refresh
+
+    reg [1:0] digit_select;   
+    reg [16:0] digit_timer;   
     
-    // Logic for controlling digit select and digit timer
+
     always @(posedge clk_100MHz or posedge reset) begin
         if(reset) begin
             digit_select <= 0;
             digit_timer <= 0; 
         end
-        else                                        // 1ms x 4 displays = 4ms refresh period
-            if(digit_timer == 99_999) begin         // The period of 100MHz clock is 10ns (1/100,000,000 seconds)
-                digit_timer <= 0;                   // 10ns x 100,000 = 1ms
+        else                              
+            if(digit_timer == 99_999) begin     
+                digit_timer <= 0;             
                 digit_select <=  digit_select + 1;
             end
             else
                 digit_timer <=  digit_timer + 1;
     end
     
-    // Logic for driving the 4 bit anode output based on digit select
+
     always @(digit_select) begin
         case(digit_select) 
-            2'b00 : digit = 4'b1110;   // Turn on ones digit
-            2'b01 : digit = 4'b1101;   // Turn on tens digit
-            2'b10 : digit = 4'b1011;   // Turn on hundreds digit
-            2'b11 : digit = 4'b0111;   // Turn on thousands digit
+            2'b00 : digit = 4'b1110;  
+            2'b01 : digit = 4'b1101;  
+            2'b10 : digit = 4'b1011;  
+            2'b11 : digit = 4'b0111; 
         endcase
     end
     
-    // Logic for driving segments based on which digit is selected and the value of each digit
+
     always @*
         case(digit_select)
             2'b00 : begin       // ONES DIGIT
